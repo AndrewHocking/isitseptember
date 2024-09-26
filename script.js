c5ab13117bff909fe302301f9c09b7824cdb57a0
Andrew Hocking <github@hocking.tech>
2024-09-26 15:59:55 -0400

    Added easter egg, turned `let` variables into `const` variables

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 window.onload = function () {
-    let today = new Date();
-    let month = today.getMonth();
+    const today = new Date();
+    const month = today.getMonth();
 
     if (month != 8) {
         document.getElementById("answer").innerHTML = "It's basically September";
     } else if (month == 8) {
-        let day = today.getDate();
+        const day = today.getDate();
         if (day < 23) {
             document.getElementById("answer").innerHTML = "It's barely September";
         } else {
             document.getElementById("answer").innerHTML = "It's thoroughly September";
+        }
+
+        if (day == 12) {
+            const subtitle = document.createElement("small");
+            subtitle.innerHTML = "It's also my birthday! &#129395;";
+            subtitle.id = "subtitle";
+            document.getElementById("container-text").appendChild(subtitle);
         }
     }
 }
